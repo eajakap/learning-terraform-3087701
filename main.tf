875e47684bb6310ff55e9518de56a45dd2312c49
Ajay Kapoor <6249202+eajakap@users.noreply.github.com>
2023-04-30 16:36:30 -0400

    fix issues

--- main.tf
+++ main.tf
@@ -45,13 +45,14 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
-  name = "blog_new"
 
+  name        = "blog"
   vpc_id      = module.vpc.public_subnets[0]
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
+
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 
 }
