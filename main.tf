5f3d62ca96a6a67bdd5ef07f9ac7079fcbb145d6
Ajay Kapoor <6249202+eajakap@users.noreply.github.com>
2023-04-30 17:25:27 -0400

    fix

--- main.tf
+++ main.tf
@@ -82,10 +82,10 @@ module "blog_alb" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.17.2"
+  version = "4.13.0"
 
   name        = "blog"
-  vpc_id      = module.blog_vpc.public_subnets[0]
+  vpc_id      = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
